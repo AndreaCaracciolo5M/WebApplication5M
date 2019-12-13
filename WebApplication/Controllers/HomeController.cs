75cf6c6205dd98a94524a95aa9085677eb11b120
Salvo Finistrella <salvo.finistrella@iispascal.it>
2019-12-13 12:11:19 +0100

    Primo commit

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Helpers;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var model = new IndexModel
+            {
+                Title = "Salvo",
+                Text = "my <strong>first</strong> web application",
+                Number = CustomRandomHelper.RandomNumber(3, 7)
+            };
+            model.Articles = DatabaseHelper.GetAllArticles();
+
+            return View(model);
+        }
+    }
+}
---
+++ WebApplication/Models/IndexModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication.Models.Entities;
+
+namespace WebApplication.Models
+{
+    public class IndexModel : BaseModel
+    {
+        public string Text { get; set; }
+        public int Number { get; set; }
+        public List<Article> Articles { get; set; }
+
+
+    }
+}
